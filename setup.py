07e6b54e1876df8e23d5ce58071a5df2cc5d0765
Miriam Schnitzerlein <miriamschnitzerlein@01-zar-grp-0013.clients.int.mpl.mpg.de>
2026-05-07 13:42:35 +0200

    created setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MacrophageMorphodynamics',
+    version='2.0.1',
+    packages=['macrophage_morphodynamics'],
+    url='',
+    license='MIT license',
+    author='miriamschnitzerlein',
+    author_email='',
+    description=''
+)
